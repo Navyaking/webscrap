d0298fadd51ec16c5a81eedd837835dcb8c2abaa
unknown <nksyt43@gmail.com>
2024-04-11 19:59:38 +0530

    second commit

--- carbon/items.py
+++ carbon/items.py
@@ -9,4 +9,11 @@ import scrapy
 class CarbonItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+
+    product_name=scrapy.Field()
+    product_size=scrapy.Field()
+    product_price=scrapy.Field()
+    product_imagelink=scrapy.Field()
+    product_designer=scrapy.Field()
+
     pass
--- carbon/spiders/carbon.py
+++ carbon/spiders/carbon.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import CarbonItem
 
 
 class Carbon(scrapy.Spider):
@@ -7,4 +8,19 @@ class Carbon(scrapy.Spider):
 
 
     def parse(self, response):
-        
+        items = CarbonItem()
+
+
+        product_name=response.css('.Heading a::text').extract()  
+        product_price=response.css('.Price').css('::text').extract()
+        product_size=response.css('a.add-size-to-cart::text').extract()
+        product_imagelink=response.css('.ProductItem__Image--alternate::attr(src)').extract()
+        product_designer=response.css('.ProductItem__Designer::text').extract() 
+
+        items['product_designer']=product_designer 
+        items['product_name']=product_name 
+        items['product_price']=product_price
+        items['product_size']=product_size
+        items['product_imagelink']=product_imagelink 
+
+        yield items
